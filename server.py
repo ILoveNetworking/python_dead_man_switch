88cce0852ac9c148efbfb60a31ad0723817093d0
ILoveNetworking <v1p33r4l1fe@protonmail.com>
2023-09-20 00:16:38 -0400

    Added mail sending -> server.py

--- server.py
+++ server.py
@@ -5,7 +5,26 @@ import json
 import argparse
 from time import sleep
 from datetime import datetime, timezone
-# import smtp library here
+import textwrap
+import smtplib
+from select import select
+
+def send_email(FROM : str, TO : str, SUBJECT : str, TEXT : str, SERVER : str, PORT : int, CREDS : dict):
+    """
+    This function used to send the email to specified user from specified email
+    """
+    message = textwrap.dedent("""\
+        From: %s
+        To: %s
+        Subject: %s
+        %s
+        """ % (FROM, ", ".join(TO), SUBJECT, TEXT))
+    # Send the mail
+    server = smtplib.SMTP(SERVER, PORT)
+    server.starttls()
+    server.login(CREDS["username"], CREDS["password"])
+    server.sendmail(FROM, TO, message)
+    server.quit()
 
 def load_config(file_path : str) -> dict:
     # Get password, time to expect connection (in utc format) (begin - end), message contents from config file
@@ -17,7 +36,7 @@ def load_config(file_path : str) -> dict:
         print("[!] File not exists!")
         return None
 
-def handle_connect(sock_type : socket.SocketKind, connection_data : tuple, time_limits : list, password : str) -> None:
+def handle_connect(sock_type : socket.SocketKind, connection_data : tuple, time_limits : list, password : str, smtp_cfg : dict) -> None:
     """
     This function used to handle the client TCP or UDP connection and recieve data, in this case user specified password
     """
@@ -39,12 +58,18 @@ def handle_connect(sock_type : socket.SocketKind, connection_data : tuple, time_
             # Which means it is exact timespan when connection is expected
             if sock_type == socket.SOCK_STREAM:
                 client, addr = server.accept()
-                message = client.recv(1024)
-                message = message.decode().strip().replace("\n", "")
-                if message == password: # Get password from config
-                    password_entered = True
-                    print(f"From: {addr}, Message: {message}")
-                    client.close()
+                # Preventing the blocking
+                r, _, _ = select([client], [], [])
+                if client in r:
+                    message = client.recv(1024)
+                    message = message.decode().strip().replace("\n", "")
+                    if message == password: # Get password from config
+                        client.send(b"OK")
+                        password_entered = True
+                        print(f"From: {addr}, Message: {message}")
+                        client.close()
+                    else:
+                        client.close()
             else:
                 message, addr = server.recvfrom(1024)
                 message = message.decode().strip().replace("\n", "")
@@ -56,14 +81,34 @@ def handle_connect(sock_type : socket.SocketKind, connection_data : tuple, time_
             # Which means time is expired
             print("Time expired, sending the messages from smtp to specified accounts!")
             # Send the mails to specified users
+            # Looping through the target users
+            for t_user in smtp_cfg["clients"]:
+                send_email(
+                    smtp_cfg["from"],
+                    t_user,
+                    smtp_cfg["subject"],
+                    smtp_cfg["message"],
+                    smtp_cfg["smtp_server_name"],
+                    smtp_cfg["smtp_server_port"],
+
+                )
             
         elif password_entered:
             # Password was entered and it was exact same as config, waiting next day and revert password entered value
             print("[+] Password entered successfully, waiting the next day!")
             while str(current_time.hour) != "00":
-                sleep(10)
+                # Sleeping for 1 hour
+                sleep(60 * 60 * 1000)
                 current_time = datetime.now(timezone.utc).time()
             password_entered = False
+        else:
+            # Waiting for time when password must be entered
+            print("[+] Waiting time to expect the password")
+            while str(current_time.hour) + ":" + str(current_time.minute) != str(time_limits[0].hour) + ":" + str(time_limits[0].minute):
+                # Sleep for 1 minute
+                sleep(60 * 1000)
+                current_time = datetime.now(timezone.utc).time()
+
 
 
 def main() -> None:
@@ -86,7 +131,18 @@ def main() -> None:
         datetime.strptime(cfg["time_span"][0], "%H:%M:%S.%f").time(),
         datetime.strptime(cfg["time_span"][1], "%H:%M:%S.%f").time()
     ]
-    handle_connect(socket_type, conn_data, time_limits, cfg["password"])
+
+    smtp_data = {
+        "smtp_server_name": cfg["smtp_server_name"],
+        "smtp_server_port": cfg["smtp_server_port"],
+        "smtp_server_cred": cfg["smtp_server_cred"],
+        "subject": cfg["subject"],
+        "message": cfg["message"],
+        "from": cfg["from"],
+        "clients": cfg["clients"]
+    }
+
+    handle_connect(socket_type, conn_data, time_limits, cfg["password"], smtp_data)
     
 
 if __name__ == "__main__":
